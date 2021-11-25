756767c008efdf48e3898f9e38cbb965e7fc6821
Mircea Dan <danaurelianmircea@yahoo.com>
2021-11-26 00:53:32 +0200

    Laborator4_PSSC_DanMirceaAurelian

---
+++ Lucrarea-04/Laborator4_PSSC_DanMirceaAurelian/Laborator4_PSSC_DanMirceaAurelian/Domain/Repositories/IOrderLinesRepository.cs
@@ -0,0 +1,18 @@
+﻿using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Laborator4_PSSC_DanMirceaAurelian.Domain.ShoppingCarts;
+
+
+namespace Laborator4_PSSC_DanMirceaAurelian.Domain.Repositories
+{
+    public interface IOrderLinesRepository
+    {
+        TryAsync<List<CalculatedShoppingCart>> TryGetExistingOrderLines();
+
+        TryAsync<Unit> TrySaveOrderLines(PaidShoppingCarts shoppingCarts);
+    }
+}
---
+++ Lucrarea-04/Laborator4_PSSC_DanMirceaAurelian/Laborator4_PSSC_DanMirceaAurelian/ShoppingCartsContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Laborator4_PSSC_DanMirceaAurelian.Models;
+
+namespace Laborator4_PSSC_DanMirceaAurelian
+{
+    public class ShoppingCartsContext : DbContext
+    {
+        public ShoppingCartsContext(DbContextOptions<ShoppingCartsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<OrderHeaderDto> OrderHeaders { get; set; }
+
+        public DbSet<OrderLineDto> OrderLines { get; set; }
+
+        public DbSet<ProductDto> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductDto>().ToTable("Product").HasKey(p => p.ProductId);
+            modelBuilder.Entity<OrderLineDto>().ToTable("OrderLineId").HasKey(ol => ol.OrderLineId);
+            modelBuilder.Entity<OrderHeaderDto>().ToTable("OrderId").HasKey(oh => oh.OrderId);
+        }
+    }
+}
